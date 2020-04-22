7eb4f5cf3b3678ddf9666751a1144043d14d9310
badido18 <hm_boudis@esi.dz>
2020-04-22 17:29:38 +0100

    Init

---
+++ client/src/components/Home.js
@@ -0,0 +1,16 @@
+import React , {Fragment} from 'react'
+import {Router,Route} from 'react-router'
+
+const Home = () =>  {
+    return (
+        <Fragment>
+            
+            <Router>
+                <Route></Route>
+                <Route></Route>
+            </Router>
+        </Fragment>
+    )
+}
+
+export default Home
