e30ab5c180d307b72cd83bbfe01574175220a588
badido18 <hm_boudis@esi.dz>
2020-05-10 21:50:58 +0100

    Feat : links style

--- client/src/components/dev/Links.js
+++ client/src/components/dev/Links.js
@@ -1,15 +1,30 @@
 import React from 'react'
+import gitlogo from '../../assets/imgs/github-1.svg'
+import gittxt from '../../assets/imgs/github-2.svg'
+import hkrlogo from '../../assets/imgs/hackrrank.svg'
+import hkrtxt from '../../assets/imgs/hackerrank.svg'
+import Inlogo from '../../assets/imgs/linkedin-icon-2.svg'
+import Intxt from '../../assets/imgs/LinkedIN.svg'
+
 
 const Links = () => {
     return (
-        <div className="Container">
+        <div className="Container li">
                 <div className="Links">
-                    <img src="" alt=""/>
-                    <button className="link">
-                        <img src="" alt=""/>
+                    <button  className="link">
+                        <img className="Linkogo" src={hkrlogo} alt=""/>
+                        <img className="Linktxt" src={hkrtxt} alt=""/>
                     </button>
-                    <h1 className="Title">Links</h1>
+                    <button className="link">
+                        <img className="Linkogo" src={gitlogo} alt=""/>
+                        <img className="Linktxt" src={gittxt} alt=""/>
+                    </button>
+                    <button className="link">
+                        <img className="Linkogo" src={Inlogo} alt=""/>
+                        <img className="Linktxt" src={Intxt} alt=""/>
+                    </button>
                 </div>
+                <h1 className="Title">Links</h1>
         </div>
     )
 }
