6f3fb52f05edf87ce7e19afe570bab803c8eb489
badido18 <hm_boudis@esi.dz>
2020-05-13 23:50:06 +0100

    Feat : Projects

--- client/src/components/dev/Projects.js
+++ client/src/components/dev/Projects.js
@@ -1,18 +1,33 @@
 import React from 'react'
-
+import AT from '../../assets/imgs/AT.png'
+import AP from '../../assets/imgs/APPS2EE.png'
+import TMC from '../../assets/imgs/TMC.png'
+import LI from '../../assets/imgs/MyLists.png'
 const Projects = () => {
     return (
-        <div className="Container">
+        <div className="Container li">
                 <div className="Projects">
-                    <img src="" alt=""/>
                     <div className="slider">
                         <div className="repoholder">
-                            <img src="" alt=""/>
+                            <p>Task Manager and Cleaner</p>
+                            <img src={TMC} alt=""/>
+                        </div>
+                        <div className="repoholder">
+                            <img src={AT} alt=""/>
+                            <p>Algerian Threads</p>
+                        </div>
+                        <div className="repoholder">
+                            <img src={LI} alt=""/>
+                            <p>My Lists</p>
+                        </div>
+                        <div className="repoholder">
+                            <img src={AP} alt=""/>
+                            <p>APP S2EE</p>
                         </div>
                     </div>
                     <p className="sub">Click to access my github repository</p>
-                    <h1 className="Title">Projects</h1>
                 </div>
+                <h1 className="Title">Projects</h1>
             </div>
     )
 }
