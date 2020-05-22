9e11c29269a8ee973867093cb8ce3f4a718e84ea
badido18 <hm_boudis@esi.dz>
2020-05-22 02:27:57 +0100

    Fix : Titles

--- client/src/components/dev/Projects.js
+++ client/src/components/dev/Projects.js
@@ -8,19 +8,19 @@ const Projects = () => {
         <div className="Container li" id="Projects">
                 <div className="Projects">
                     <div  className="slider" >
-                        <div className="repoholder">
+                        <div onClick={()=> window.open("https://github.com/badido18/Task-manager-and-cleaner", "_blank")} className="repoholder">
                             <p>Task Manager and Cleaner</p>
                             <img src={TMC} alt=""/>
                         </div>
-                        <div className="repoholder">
+                        <div onClick={()=> window.open("https://github.com/badido18/Algerian-Threads-Website", "_blank")} className="repoholder">
                             <img src={AT} alt=""/>
                             <p>Algerian Threads</p>
                         </div>
-                        <div className="repoholder">
+                        <div onClick={()=> window.open("https://github.com/badido18/My-Lists-Web", "_blank")} className="repoholder">
                             <img src={LI} alt=""/>
                             <p>My Lists</p>
                         </div>
-                        <div className="repoholder">
+                        <div onClick={()=> window.open("https://github.com/badido18/AppS2EE2019", "_blank")} className="repoholder">
                             <img src={AP} alt=""/>
                             <p>APP S2EE</p>
                         </div>
